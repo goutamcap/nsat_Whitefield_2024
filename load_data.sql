aff3ae3d88964e9cdf2fb6e0ccc4cb2d866c9c94
goutamcap <goutampanda1997@gmail.com>
2025-09-21 21:06:07 +0530

    Add files via upload

---
+++ load_data.sql
@@ -0,0 +1,9 @@
+LOAD DATA INFILE '/Users/goutampanda/Desktop/Analytics /Power Bi Project '
+INTO TABLE NSAT_Whitefield
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(Sl_No, School_Name, Distance, Fees, Category, Board, NSAT_Date, ME_Name, QP_Set,
+ Class_6, Class_7, Class_8, Class_9, Class_10, Class_11_J, Class_11_N, Class_12_N, Class_12_J,
+ Total, OMR_from_School, Clean_Result);
